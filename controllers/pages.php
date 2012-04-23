3bf7264172fb49e4832543bc3b36d3869d05684b
Chris Dosé <chris.dose@gmail.com>
2012-04-23 09:47:24 -0500

    A dynamic pages controller.

--- controllers/pages.php
+++ controllers/pages.php
@@ -1,11 +1,20 @@
 <?php
 
-get('/pages/page1', function() use ($html) {
-	$page = view('pages/page1');
-	return $html('Simple Page 1', $page());
-});
+$html = layout('html');
 
-get('/pages/page2', function() use ($html) {
-	$page = view('pages/page2');
-	return $html('Simple Page 2', $page());
+get('/pages/:page', function($page) use ($html) {
+	$titles = array(
+		'page1' => 'Simple Page 1',
+		'page2' => 'Simple Page 2'
+	);
+
+	if(!file_exists('../views/pages/'.$page.'.php')) {
+		$debug = getConfigVar('core', 'debug', true);
+		if($debug) { noSuchView('../views/pages/'.$page); }
+		else { error404(); }
+	}
+
+	$title = $titles[$page];
+	$page = view('pages/'.$page);
+	return $html($title, $page());
 });
